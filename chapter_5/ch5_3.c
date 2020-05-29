a925ead1219ebc42040a956b488eecd87a657b9e
Alex <aap5869@g.rit.edu>
2020-05-29 18:41:21 -0400

    Adding ch5_3.c

---
+++ chapter_5/ch5_3.c
@@ -0,0 +1,28 @@
+/* C Programming A Modern Approach
+ * Chapter 5
+ * Exercise 3                                                                    
+ *                                                                             
+ * Modified version of the broker.c program of Secion 5.2 by making both of the
+ * following changes:
+ *
+ * (a) Asks the user to enter the number of shares and the price per share, 
+ * instead of the value of the trade
+ *
+ * (b) Adds statements that compute the commision charged by a rival broker
+ * ($33 plus 3 cents per share for fewer than 2000 shares; $33 plus 2 cents per
+ * share for 2000 shares or more). Display the rival's commission as well as the
+ * commission charged by the original broker.
+ *
+ * Author: Alex Perkins
+ * Date: 28-May-2020
+ */   
+
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void)
+{
+    uint16_t hour = 0, minute = 0;
+
+    return 0;
+}
