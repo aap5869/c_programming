e74b104a6172cb84a2947c78c62e53c3063666ba
Alex <chileboy@pop-os.localdomain>
2020-05-29 20:27:02 -0400

    Adding ch5_3.c

--- chapter_5/ch5_3.c
+++ chapter_5/ch5_3.c
@@ -22,7 +22,45 @@
 
 int main(void)
 {
-    uint16_t hour = 0, minute = 0;
+    uint16_t numShares       = 0;
+    float    sharePrice      = 0.0f;
+    float    rivalCommission = 0.0f;
+    float    origCommission  = 0.0f;
+    float    value           = 0.0f;
+
+    printf("Enter the number of shares: ");
+    if (scanf("%hu", &numShares) != 1)
+        return 1;
+
+    printf("Enter the price per share: ");
+    if (scanf("%f", &sharePrice) != 1)
+        return 2;
+
+    value = numShares * sharePrice;
+
+    if (value < 2500.00f)
+        origCommission = 30.00f + 0.017f * value;
+    else if (value < 6250.00f) 
+        origCommission = 56.00f + 0.0066f * value;
+    else if (value < 20000.00f)
+        origCommission = 76.00f + 0.0034f * value;
+    else if (value < 50000.00f)
+        origCommission = 100.00f + 0.0022f * value;
+    else if (value < 500000.00f)
+        origCommission = 155.00f + 0.0011f * value;
+    else
+        origCommission = 255.00f + 0.0009f * value;
+
+    if (origCommission < 39.00f)
+        origCommission = 39.00f;
+
+    if (numShares < 2000)
+        rivalCommission = 33.00f + 0.03f * numShares;
+    else if (numShares >= 2000)
+        rivalCommission = 33.00f + 0.02f * numShares;
+
+    printf("Original broker's commission: %.2f\n", origCommission);
+    printf("Rival broker's commission: %.2f\n", rivalCommission);
 
     return 0;
 }
